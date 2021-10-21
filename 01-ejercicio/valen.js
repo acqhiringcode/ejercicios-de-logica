23dba1871fbc5a5ffdd9b93a9049d3c11394f400
valendw <nvalentino232@gmail.com>
2021-10-21 15:45:45 -0300

    Update valen.js
    ejercicio valen y jero

--- 01-ejercicio/valen.js
+++ 01-ejercicio/valen.js
@@ -1,1 +1,7 @@
 // Crear una funcion que devuelva por consola los numeros del 1 al 10
+
+function devolverNumero() {
+    console.log("devolverNumero");
+    for (let i = 1; i <= 10; i ++);
+    console.log(i);
+}
